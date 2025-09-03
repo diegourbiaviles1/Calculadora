33b8f50488bfb8cd88cb31892c01b76662da8558
THEGABOALE <garciaga@uamv.edu.ni>
2025-09-02 23:52:00 -0600

    seleccion del metodo para la matriz

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ from utils.display import mostrar_sistema_ecuaciones
 from utils.gauss_jordan import gauss_jordan
 
 def main():
-    matriz_lista = Matrix.make_matrix(3, 3)
-    matriz = Matrix(matriz_lista)    
+    matriz_lista = Matrix.make_matrix(3, 4)
+    matriz = Matrix(matriz_lista)   
+     
+    print("\n")
     print(matriz)
     
     mostrar_sistema_ecuaciones(matriz)
+    
+    print("\nSeleccione el método que quiera usar:")
+    print("1. Método Escalonado - Eliminación Gaussiana")
+    print("2. Salida")
+        
+    opcion = int(input("R: "))
+        
+    if opcion == 1:
+        gauss_jordan(matriz)
+    elif opcion == 2:
+        print("Saliendo...")
+        return
+    else:
+        print("Opción no válida.")
     
 if __name__ == "__main__":
     main()
