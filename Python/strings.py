a83d3c84ad9bf616516dd9bb63c2724f8930d04a
Bastian-18 <lsbarreto@uamv.edu.ni>
2025-11-03 16:25:41 -0600

    Creacion de la Clase autor para la factura

---
+++ Python/strings.py
@@ -0,0 +1,70 @@
+#In this class we learn how to use the strings in python
+
+#str = 'This is a string with simple quote'
+#print(str)
+
+#str1 = "This is a string create using the doble quotes"
+#print(str1)
+
+#str2 = """"this is a string using a multiple quotes, so you 
+#know how to look this type of string"""
+#print(str2)
+
+#_______________________________________________________________________________#
+#Using variables in python with the f-strings
+#name = "Mike"
+#job = "What are you doing?"
+#Greetings =  f"How are you, {name} {job}"
+#print("hello wordl"); 
+
+
+#================================================================================================
+# String indexin and splitting
+#How to acces caracter by using positive index number and also usign negative index number. 
+#str = "Python"
+#print(str[0])
+#print(str[1])
+#print(str[2])
+#print(str[3])
+#print(str[7]) in this case, we got a error, because "7" dont exist in the word "Python"
+#================================================================================================
+#Strings splitting 
+#Given string 
+
+#anguage = "Programming"
+#print(language[0:])
+#print(language[0:3])
+#print(language[0:4])
+#print(language[0:0:2])
+#print(language[:5])
+#================================================================================================
+#Re - Assingning strings in python.
+#str = "Hello world"
+
+#str[0] = "H"
+#print(str)
+
+#str = "Maderfuker"
+#print(str)
+#================================================================================================
+#Formating of strings 
+
+#----------------------------------------Usign curly Braces
+#print(" Both {} and {} are software engeniering students".format("Lenin", "Johaneris"))
+
+#----------------------------------------Positional Arguments
+#print("{0} and {1} are the best football players in this world".format("Messi", "Ronaldo"))
+
+#----------------------------------------Keywords Arguments
+#print("{A}, {B} and {C} are programming lenguagues".format(A = "Python" , B = "NMAP", C = "Wireshark"))
+
+#================================================================================================
+#Changing case in string with methods: .lower(),  .upper() and .title()
+#----------------------------------------.Tittle()
+#name = "H.P Lovecraft"
+#print(name.title())
+#----------------------------------------.lower() and .upper()
+#Country = input("Where are you living? ")
+#print(f"I am living in {Country.lower()}")
+#print(f"I am living in {Country.upper()}")
+#================================================================================================#
